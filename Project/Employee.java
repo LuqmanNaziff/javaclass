06137fbe11f9511fdf6a4f776c2377a35bb2adf4
LuqmanNaziff <Luqq99@gmail.com>
2024-09-24 16:14:41 +0800

    Employee System

---
+++ Project/Employee.java
@@ -0,0 +1,60 @@
+package Project;
+
+public class Employee {
+    String name;
+    double baseSalary = 0;
+    double healthAllowance = 0;
+    double transportAllowance = 0;
+    double bonus = 0;
+    double totalSalary = 0;
+    double totalBonus = 0;
+    double actualSalary = 0;
+    
+    public Employee(String name, double baseSalary){
+        this.name = name;
+        this.baseSalary = baseSalary;
+        this.healthAllowance = 5000;
+        this.transportAllowance = 300;
+        this.bonus = assignBonus();
+        this.actualSalary = calculateTotalSalary();
+
+
+    }
+
+    public String getName(){
+        return name;
+    }
+// else if it is 
+    public double assignBonus(){          
+            if(baseSalary >= 50000){
+                bonus = 10.0;
+                
+                // return bonus;
+            }else if(baseSalary >= 30000){
+                bonus = 5.0;
+                
+                // return bonus;
+            }else{
+                bonus = 0.0;
+                // return bonus;
+        }
+        totalBonus = (baseSalary * bonus / 100); 
+        return totalBonus;
+    }
+
+    public double calculateTotalSalary(){
+        totalSalary = baseSalary + healthAllowance + transportAllowance + totalBonus;
+        return totalSalary;
+    }
+    
+
+    public void displayDetails(){
+        System.out.println("name is: " + name);
+        System.out.println("Base Salary is : " + baseSalary);
+        System.out.println("Health Allowance is : " + healthAllowance);
+        System.out.println("Transport Allowance is : " + transportAllowance);
+        System.out.println("Total Salary: " + actualSalary);
+    }
+
+
+}
